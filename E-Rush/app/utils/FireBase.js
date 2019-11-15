d943283f357715339bfbd0c500c7e928370624c5
StarNiGHTS <xpkmn64x@gmail.com>
2019-11-15 02:07:27 -0600

    Configurar Firebase

---
+++ E-Rush/app/utils/FireBase.js
@@ -0,0 +1,9 @@
+export default firebaseConfig = ({
+  apiKey: "AIzaSyAqzuERyibTjJ0qd3yDk9_kjnTgN5zg1Y4",
+  authDomain: "e-rush-95dd4.firebaseapp.com",
+  databaseURL: "https://e-rush-95dd4.firebaseio.com",
+  projectId: "e-rush-95dd4",
+  storageBucket: "e-rush-95dd4.appspot.com",
+  messagingSenderId: "354704680470",
+  appId: "1:354704680470:web:8b50b03e37f224dfe4f737"
+});
