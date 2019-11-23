3797fbca9cf57fcba5582af1fe040dad2ec6364b
StarNiGHTS <xpkmn64x@gmail.com>
2019-11-23 01:29:18 -0600

    Login y Logout

---
+++ E-Rush/app/screens/MyAccount/Login.js
@@ -0,0 +1,138 @@
+import React, { Component } from "react";
+import { StyleSheet, View, Text, ActivityIndicator } from "react-native";
+import { Image, Button } from "react-native-elements";
+import Toast, { DURATION } from "react-native-easy-toast";
+
+import t from "tcomb-form-native";
+const Form = t.form.Form;
+import { LoginOptions, LoginStruct } from "../../components/forms/Login";
+
+import * as firebase from "firebase";
+
+export default class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loginStruct: LoginStruct,
+      loginOptions: LoginOptions,
+      loginData: {
+        email: "",
+        password: ""
+      },
+      loginErrorMessage: ""
+    };
+  }
+
+  login = () => {
+    //console.log("Haciendo Login");
+    //console.log(this.state.loginData);
+    const validate = this.refs.loginForm.getValue();
+
+    if (!validate) {
+      this.setState({
+        loginErrorMessage: "El email y/o contraseña son invalidos"
+      });
+      //console.log("Formulario Incorrecto");
+    } else {
+      this.setState({
+        loginErrorMessage: ""
+      });
+
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(validate.email, validate.password)
+        .then(() => {
+          this.refs.toastLogin.show("Login correcto", 200, () => {
+            this.props.navigation.goBack();
+          });
+          //console.log("Login Correcto");
+        })
+        .catch(erro => {
+          this.refs.toastLogin.show("Email y/o contraseña incorrectos", 2500);
+          //console.log("Login Incorrecto");
+        });
+    }
+
+    //console.log(validate);
+  };
+
+  onChangeFormLogin = formValue => {
+    //console.log("Change Form Login...");
+    //console.log(formValue);
+    this.setState({
+      loginData: formValue
+    });
+  };
+
+  render() {
+    const { loginStruct, loginOptions, loginErrorMessage } = this.state;
+
+    return (
+      <View style={styles.viewBody}>
+        <Image
+          source={require("../../../assets/img/5-tenedores-letras-icono-logo.png")}
+          containerStyle={styles.containerLogo}
+          style={styles.logo}
+          PlaceholderContent={<ActivityIndicator />}
+          resizeMode="contain"
+        />
+        <View style={styles.viewForm}>
+          <Form
+            ref="loginForm"
+            type={loginStruct}
+            options={loginOptions}
+            value={this.state.loginData}
+            onChange={formValue => this.onChangeFormLogin(formValue)}
+          />
+          <Button
+            buttonStyle={styles.buttonLoginContainer}
+            title="Login"
+            onPress={() => this.login()}
+          />
+          <Text style={styles.loginErrorMessage}> {loginErrorMessage}</Text>
+        </View>
+
+        <Toast
+          ref="toastLogin"
+          position="bottom"
+          positionValue={300}
+          fadeInDuaration={750}
+          fadeOutDuration={1000}
+          opacity={0.8}
+          textStyle={{ color: "#fff" }}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  viewBody: {
+    flex: 1,
+    marginLeft: 40,
+    marginRight: 40,
+    marginTop: 40
+  },
+  containerLogo: {
+    alignItems: "center"
+  },
+  logo: {
+    width: 300,
+    height: 150
+  },
+  viewForm: {
+    marginTop: 50
+  },
+  buttonLoginContainer: {
+    backgroundColor: "#00a680",
+    marginTop: 20,
+    marginLeft: 10,
+    marginRight: 10
+  },
+  loginErrorMessage: {
+    color: "#f00",
+    textAlign: "center",
+    marginTop: 20
+  }
+});
--- E-Rush/app/screens/MyAccount/MyAccount.js
+++ E-Rush/app/screens/MyAccount/MyAccount.js
@@ -7,19 +7,63 @@ import {
 } from "react-native";
 import { Button } from "react-native-elements";
 
+import * as firebase from "firebase";
+
 export default class MyAccount extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      login: false
+    };
+  }
+
+  async componentDidMount() {
+    await firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          login: true
+        });
+      } else {
+        this.setState({
+          login: false
+        });
+      }
+    });
+  }
+
   goToScreen = nameScreen => {
     //console.log(nameScreen);
     this.props.navigation.navigate(nameScreen);
   };
 
+  logout = () => {
+    firebase.auth().signOut();
+    //console.log("Cerrar sesion...");
+  };
+
   render() {
-    return (
-      <View style={styles.viewBody}>
-        <Text>MyAccount Screen...</Text>
-        <Button title="Registro" onPress={() => this.goToScreen("Register")} />
-      </View>
-    );
+    const { login } = this.state;
+
+    if (login) {
+      return (
+        <View style={styles.viewBody}>
+          <Text>Estas logeado correctamente...</Text>
+          <Button title="Cerrar sesión" onPress={() => this.logout()} />
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.viewBody}>
+          <Text>MyAccount Screen...</Text>
+          <Button
+            title="Registro"
+            onPress={() => this.goToScreen("Register")}
+          />
+          <Button title="Login" onPress={() => this.goToScreen("Login")} />
+        </View>
+      );
+    }
   }
 }
 
